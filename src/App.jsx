155a83f807dfdb5cf1d560b8dd679572ad519aac
Harshal Khairnar <harshalkhairnar7244@gmail.com>
2025-05-23 11:21:20 +0530

    making progress bar scalable

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,21 @@ import ProgressBar from './components/ProgressBar'
 
 export default function App() {
   const [value, setValue] = useState(0);
+  const [success, setSuccess] = useState(false);
   useEffect(()=>{
     setInterval(() => {
       setValue((val)=>val+1);
-    }, 100);
+    }, 50);
   },[])
   return (
     <div className='app'>
       <span>Progress Bar</span>
-      <ProgressBar value={value}/>
+      <ProgressBar
+       value={value}
+       onStart={()=>{}}
+       onComplete={()=>{setSuccess(true)}}
+      />
+      <span>{success?"Complete!":"Loading..."}</span>
     </div>
   )
 }
--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,20 +1,30 @@
 import { useEffect, useState } from "react";
 import "../App.css";
+import { MAX, MIN } from "../constants";
 
-const ProgressBar = ({ value = 0 }) => {
+const ProgressBar = ({ value = 0,onComplete=()=>{} }) => {
   const [percent, setPercent] = useState(value);
   useEffect(() => {
-    setPercent(Math.min(100, Math.max(value, 0)));
+    setPercent(Math.min(MAX, Math.max(value, MIN)));
+    if(value>=MAX){
+      onComplete();
+    }
   }, [value]);
   return (
     <div className="progress">
       <span style={{ color: percent > 49 ? "white" : "black" }}>
         {percent.toFixed()}%
       </span>
-      <div style={{ width: `${percent}%` }} role="progressbar"
-      aria-valuemax={100}
-      aria-valuemin={0}
-      aria-valuenow={percent}
+      <div
+        // style={{ width: `${percent}%` }}
+        style={{
+          transform: `scaleX(${percent / MAX})`,
+          transformOrigin: "left",
+        }}
+        role="progressbar"
+        aria-valuemax={MAX}
+        aria-valuemin={MIN}
+        aria-valuenow={percent}
       ></div>
     </div>
   );
